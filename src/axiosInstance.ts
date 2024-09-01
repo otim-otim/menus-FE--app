1dc9bef725951d9dc9285568100dd31e4794f871
Otim Isaac <isaacomega16@gmail.com>
2024-09-01 05:23:46 +0300

    update backend url reference

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://api.example.com',
+  baseURL: 'http://127.0.0.1:8000/api',
   // Other custom configurations
 });
 
