3647c6ce5c5da383725655886b86ebd572a00f4c
Otim Isaac <isaacomega16@gmail.com>
2024-09-03 00:40:59 +0300

    add update and store menu service functions

--- src/services/Menus.service.ts
+++ src/services/Menus.service.ts
@@ -42,18 +42,18 @@ async function storeMenu(menuData : { name: string, parentId?: string }){
     }
 }
 
-export function storeNewMenu(){
+export function storeNewMenu(reqData :{ name: string, parentId?: string } ){
     try {
         const menus = useRecoilValue(menusState)
         const setMenusState = useSetRecoilState(menusState)
         const setMenuStoringState = useSetRecoilState(menuStoringState)
         const {data, status, error} = useQuery({
             queryKey: ['store-menu'],
-            queryFn: storeMenu,
+            queryFn: () =>  storeMenu(reqData),
           })
 
           setMenuStoringState(status)
-                    
+
           if(status === 'success') {
             const menu = data?.data
             if (menu.parent){
@@ -80,3 +80,53 @@ export function storeNewMenu(){
     }
 
 }
+
+
+async function updateMenu(menuId: string,menuData : { name: string }){
+    try {
+        const response= await axiosInstance.post(`/menus/${menuId}`, menuData)
+        return response
+    } catch (error) {
+        
+    }
+}
+
+export function updateSelectedMenu(menuId: string,updateReqData :{ name: string } ){
+    try {
+        const menus = useRecoilValue(menusState)
+        const setMenusState = useSetRecoilState(menusState)
+        const setMenuStoringState = useSetRecoilState(menuStoringState)
+        const {data, status, error} = useQuery({
+            queryKey: ['store-menu'],
+            queryFn: () => updateMenu(menuId,updateReqData),
+          })
+
+          setMenuStoringState(status)
+
+          if(status === 'success') {
+            const menu = data?.data
+            if (menu.parent){
+                const parent = menus.find( menu => menu.id ==  menu.parent)
+                if(parent){
+                    parent.children = [...parent?.children,menu]
+                    const updatedMenu  = menus.map(menu => {
+                        if(menu.id == parent.id)
+                            return parent
+                        return menu
+                    })
+                    setMenusState(updatedMenu)
+
+                }
+                
+            }
+            else 
+              setMenusState([...menus,menu])
+          }
+
+
+    } catch (error) {
+        
+    }
+
+}
+
