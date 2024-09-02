78fe703f6f705b5b06d5dd1ba2b588af074575a5
Otim Isaac <isaacomega16@gmail.com>
2024-09-03 02:43:50 +0300

    import create menu component

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header"
 import Menus from "@/components/menus/Menus"
 import PageTitle from "@/components/PageTitle"
 import ParentMenuDropdown from "@/components/menus/ParentMenuDropdown"
+import CreateMenu from "@/components/menus/CreateMenu"
 
 export default function MenuPage() {
     return (
@@ -9,7 +10,16 @@ export default function MenuPage() {
         <Header />
   <PageTitle />
   <ParentMenuDropdown />
-    <Menus />
+  <div className="flex justify-between">
+    <div className="w-1/2" >
+  <Menus  />
+
+    </div>
+    <div className="w-1/2" >
+  <CreateMenu />
+
+    </div>
+</div>
         </>
     )
 }
